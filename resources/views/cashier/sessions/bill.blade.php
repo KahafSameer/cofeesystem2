6f8731cbe3865809bbff2b8f2569e2868ee8fbac
Kahaf Sameer <sameerkahaf920@gmail.com>
2026-09-02 00:55:00 +0500

    bill updates

--- resources/views/cashier/sessions/bill.blade.php
+++ resources/views/cashier/sessions/bill.blade.php
@@ -14,6 +14,8 @@
         <div class="receipt-logo-wrap">
             @if(file_exists(public_path('images/logo.png')))
                 <img src="{{ asset('images/logo.png') }}" alt="V8 Cafe" class="receipt-logo">
+            @elseif(file_exists(public_path('images/logo.svg')))
+                <img src="{{ asset('images/logo.svg') }}" alt="V8 Cafe" class="receipt-logo">
             @elseif(file_exists(resource_path('logo.png')))
                 <img src="{{ asset('logo.png') }}" alt="V8 Cafe" class="receipt-logo">
             @else
